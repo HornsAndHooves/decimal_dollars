8cc2876ee80f148b9f66e7d6112f512e62c6a6bd
Roman Trofimov <trofimov.roman@gmail.com>
2014-05-13 15:16:34 +0300

    More spec, minor fixes

--- spec/lib/decimal_dollars/money_spec.rb
+++ spec/lib/decimal_dollars/money_spec.rb
@@ -40,4 +40,15 @@ describe DecimalDollars::Money do
       }.to raise_error(RuntimeError, 'Money cannot be raised to any power')
     end
   end
+
+  describe "#round_float" do
+    it "rounds correctly" do
+      expect(described_class.new(1.251).to_f).to  eq(1.25)
+      expect(described_class.new(1.255).to_f).to  eq(1.25)
+      expect(described_class.new(1.2551).to_f).to eq(1.26)
+      expect(described_class.new(1.2555).to_f).to eq(1.26)
+      expect(described_class.new(1.2556).to_f).to eq(1.26)
+      expect(described_class.new(1.256).to_f).to  eq(1.26)
+    end
+  end
 end
